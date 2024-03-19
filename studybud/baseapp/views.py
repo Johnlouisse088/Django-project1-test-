b1e80a9777d4281d5c98f7ac2f6ebb1251223e5f
JLouisse088 <louissepolong890@gmail.com>
2024-03-19 11:03:36 +0800

    error encounter

--- studybud/baseapp/views.py
+++ studybud/baseapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User, Topic, Room, Message
 
 # Create your views here.
 
 def home(request):
+    query = request.GET.get("q") if request.GET.get("q") is not None else ""
+    topics = Topic.objects.all()
+    rooms = Room.objects.all()
+    room_topic = rooms.filter(
+        room__icontains = query
+    )
     context ={
-
+        "topics": topics,
+        "room_topic":room_topic,
     }
     return render(request, "home.html", context)
--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -124,9 +124,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "mystaticfiles")
+# ]
 
 
 
