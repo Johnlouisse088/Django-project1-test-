272863ad7d2d7a638adb20de9fd3f1bd3227acb3
JLouisse088 <louissepolong890@gmail.com>
2024-03-20 19:28:13 +0800

    done home template

--- studybud/baseapp/models.py
+++ studybud/baseapp/models.py
@@ -18,16 +18,19 @@ class Topic(models.Model):
         return self.name
 
 class Room(models.Model):
-    room = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, null=True)
     host = models.ForeignKey(User, on_delete=models.CASCADE)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     description = models.TextField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.room
+        return self.name
 
 class Message(models.Model):
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    message = models.CharField(max_length=255, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    messages = models.CharField(max_length=255, null=True)
 
+    def __str__(self):
+        return self.messages
 
--- studybud/baseapp/views.py
+++ studybud/baseapp/views.py
@@ -8,11 +8,14 @@ def home(request):
     query = request.GET.get("q") if request.GET.get("q") is not None else ""
     topics = Topic.objects.all()
     rooms = Room.objects.all()
+    messages = Message.objects.all()
     room_topic = rooms.filter(
-        room__icontains = query
+        name__icontains = query
     )
     context ={
         "topics": topics,
         "room_topic":room_topic,
+        "rooms": rooms,
+        "messages": messages,
     }
     return render(request, "home.html", context)
